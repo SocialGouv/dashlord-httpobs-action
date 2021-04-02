a1fa020145f94ca93cfe47951587e6e4e4d32ebe
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-04-03 01:07:47 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const core = require("@actions/core");
+
+const scan = require("./scan");
+
+// most @actions toolkit packages have async methods
+async function run() {
+  try {
+    const url = core.getInput("url");
+    const output = core.getInput("output");
+    core.info(`Running scan on ${url} ...`);
+    const results = await scan(url);
+    fs.writeFileSync(output, JSON.stringify(results));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
---
+++ src/scan.js
@@ -0,0 +1,49 @@
+const splittedUrl = require("splitted-url");
+const { default: fetch } = require("node-fetch");
+
+const INTERVAL = 20000;
+const MAX_TRIES = 20;
+
+const API_HTTP = `https://http-observatory.security.mozilla.org/api/v1`;
+
+/**
+ * Run a HTTP Mozilla scan for a given URL
+ *
+ * @param {string} url The full URL
+ *
+ * @returns {Promise<HttpScanResult>}
+ */
+const scan = (url, tries = MAX_TRIES) => {
+  const hostname = splittedUrl(url).host;
+  console.warn(`fetch mozilla HTTP API for ${url}`);
+  return fetch(`${API_HTTP}/analyze?host=${hostname}&hidden=true&rescan=true`, {
+    method: "POST",
+  })
+    .then((r) => r.json())
+    .then((result) => {
+      if (result.error) {
+        throw new Error(result.error);
+      }
+      if (result.state === "FINISHED") {
+        return fetch(`${API_HTTP}/getScanResults?scan=${result.scan_id}`, {
+          method: "GET",
+        })
+          .then((r) => r.json())
+          .then((details) => ({
+            url,
+            ...result,
+            details,
+          }));
+      } else if (tries > 0) {
+        console.warn(
+          `delay HTTP ${url} (${MAX_TRIES - tries + 1}/${MAX_TRIES})`
+        );
+        return new Promise((resolve) =>
+          setTimeout(() => scan(url, tries - 1).then(resolve), INTERVAL)
+        );
+      }
+      throw new Error(`${MAX_TRIES} failed scans for ${url}`);
+    });
+};
+
+module.exports = scan;
